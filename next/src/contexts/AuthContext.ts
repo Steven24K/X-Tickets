07b5182a01b944cbd963c59de4aa9cdf85c8786e
Steven <s.koerts2@gmail.com>
2025-09-02 13:08:18 +0200

    update nextjs

--- next/src/contexts/AuthContext.ts
+++ next/src/contexts/AuthContext.ts
@@ -1,8 +1,8 @@
 "use client"
 
+import { createContext } from "react"
 import { ApiResponse } from "@/services/StrapiClient"
 import { IsRight } from "@/types/Func"
 import { StrapiShopOwner } from "@/types/models/StrapiCollections"
-import { createContext } from "react"
 
 export const AuthContext = createContext<Promise<ApiResponse<StrapiShopOwner>>>(Promise.resolve(IsRight("Unloaded")))
--- next/src/contexts/Providers.tsx
+++ next/src/contexts/Providers.tsx
@@ -1,7 +1,7 @@
 "use client"
+import { Suspense } from "react";
 import { getCurrentUser } from "@/app/api";
 import { AuthContext } from "./AuthContext";
-import { Suspense } from "react";
 
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
